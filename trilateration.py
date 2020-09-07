ce96b38c00f97b72ea3e8ccd0903696b94d739dc
namkhang1875 <ice9966@hotmail.com>
2020-09-07 21:28:35 +0700

    add file

---
+++ trilateration.py
@@ -0,0 +1,12 @@
+import math
+x1,y1 = 6,0
+x2,y2 = 2,0
+x3,y3 = 0,0
+x4,y4 = 0,0
+
+# print(x4)
+
+def calculate():
+    d1 = math.sqrt(math.pow(x1-x)+math.pow(y1-y))
+    d2 = math.sqrt(math.pow(x2-x)+math.pow(y2-y))
+    d3 = math.sqrt(math.pow(x3-x)+math.pow(y3-y))        
